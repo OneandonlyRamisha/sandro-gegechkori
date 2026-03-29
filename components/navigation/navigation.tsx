39852fe9a05829295b86588c393ec2fed6505405
Luka Ramishvili <ramisha@Lukas-MacBook-Air.local>
2026-03-29 23:51:15 +0400

    wda

--- components/navigation/navigation.tsx
+++ components/navigation/navigation.tsx
@@ -16,6 +16,21 @@ export default function Navigation() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+  useEffect(() => {
+    if (isOpen) {
+      const scrollY = window.scrollY;
+      document.body.style.position = "fixed";
+      document.body.style.top = `-${scrollY}px`;
+      document.body.style.width = "100%";
+    } else {
+      const scrollY = document.body.style.top;
+      document.body.style.position = "";
+      document.body.style.top = "";
+      document.body.style.width = "";
+      window.scrollTo(0, parseInt(scrollY || "0") * -1);
+    }
+  }, [isOpen]);
+
   return (
     <motion.header
       className={`${style.header} ${scrolled ? style.headerScrolled : ""}`}
